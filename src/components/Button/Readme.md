dfaf616988bbc337a6b47a2d0da0445cd8561275
jshimazu <jshimazu0820@gmail.com>
2017-04-07 15:57:31 +0900

    add readme

---
+++ src/components/Button/Readme.md
@@ -0,0 +1,32 @@
+普通のボタン
+
+```
+<Button>Click Me</Button>
+```
+
+大事なボタン
+
+```
+<Button type="primary">Click Me</Button>
+```
+
+危険なボタン
+```
+<Button type="danger">Click Me</Button>
+```
+
+サイズ:小
+
+```
+<Button size="small">Click Me</Button>
+```
+
+サイズ:中
+```
+<Button>Click Me</Button>
+```
+
+サイズ:大
+```
+<Button size="small">Click Me</Button>
+```
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,15 +3,10 @@ import './style.scss';
 
 class Button extends Component {
   static propTypes = {
-    /**
-     * size 
-     */
     size: PropTypes.oneOf(['small', 'large']),
-    /**
-     * type
-     */
     type: PropTypes.oneOf(['danger', 'primary']),
   };
+
   render() {
     const type = this.props.type || 'default';
     const size = this.props.size || 'default';
